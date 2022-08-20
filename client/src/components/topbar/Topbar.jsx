41a72c8ad9ffccccc424990098a9bac59a87ef4f
Muthoni Kevin Gatamu <49341231+KevGMR@users.noreply.github.com>
2022-08-20 10:44:50 +0300

    picture resize

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,12 +1,15 @@
 import './topbar.css'
-import {Search, Person, Chat, Notifications} from '@mui/icons-material';
+import { Search, Person, Chat, Notifications } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
 // import PersonIcon from '@mui/icons-material/Person';
 
 function Topbar() {
   return (
     <div className="topbarContainer" >
           <div className="topbarLeft">
-              <span className="logo">KevSocial</span>
+              <Link to='/' style={{textDecoration: 'none'}} >
+                <span className="logo">KevSocial</span>
+              </Link>
         </div>
           <div className="topbarCenter">
               <div className="searchbar">
