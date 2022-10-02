3420d673661f90194856d72590fd5f57ce6db188
Kevin Gatamu <kevingatamumuthoni@gmail.com>
2022-10-02 14:05:13 +0300

    add slash

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -11,26 +11,24 @@ function Feed({ username }) {
   const [posts, setPosts] = useState([]);
   const { user } = useContext(AuthContext);
 
-
-
   useEffect(() => {
-      const fetchPosts = async () => {
-        const response = username
-          ? await axios.get(`/posts/profile/${username}`)
-          : await axios.get(`/posts/timeline/${user._id}`);
-        setPosts(
-          response.data.sort((p1, p2) => {
-            return new Date(p2.createdAt) - new Date(p1.createdAt);
-          })
-        );
-      };
+    const fetchPosts = async () => {
+      const response = username
+        ? await axios.get(`/posts/profile/${username}`)
+        : await axios.get(`/posts/timeline/${user._id}`);
+      setPosts(
+        response.data.sort((p1, p2) => {
+          return new Date(p2.createdAt) - new Date(p1.createdAt);
+        })
+      );
+    };
     fetchPosts();
   }, [username, user._id]);
 
   return (
     <div className="feed">
       <div className="feedWrapper">
-        {username ===user.username && <Share/> }
+        {username === user.username && <Share />}
         {/* <Share /> */}
         {posts.map((p) => {
           return <Post key={p._id} post={p} />;
--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,25 +1,25 @@
 import { createContext, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
-// const INITIAL_STATE = {
-//   user: null,
-//   isFetching: false,
-//   error: false,
-// };
-
 const INITIAL_STATE = {
-  user: {
-    _id: "63302eaafbe23b2b9f5bb2dc",
-    username: "kevin",
-    email: "kevin@gmail.com",
-    profilePicture: "person/1.jpeg",
-    followers: [],
-    followings: ["62c0653cdc9ab8bf10736058"],
-    isAdmin: false,
-  },
+  user: null,
   isFetching: false,
   error: false,
 };
+
+// const INITIAL_STATE = {
+//   user: {
+//     _id: "63302eaafbe23b2b9f5bb2dc",
+//     username: "kevin",
+//     email: "kevin@gmail.com",
+//     profilePicture: "person/1.jpeg",
+//     followers: [],
+//     followings: ["62c0653cdc9ab8bf10736058"],
+//     isAdmin: false,
+//   },
+//   isFetching: false,
+//   error: false,
+// };
 
 export const AuthContext = createContext(INITIAL_STATE);
 
